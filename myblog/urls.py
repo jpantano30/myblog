91c32dd7c47d2bd7fdffceb8f4ef4ec3997b8665
Jena <jpantano30@gmail.com>
2024-06-18 14:14:03 -0400

    .

--- myblog/urls.py
+++ myblog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import routers 
 from blog.views import BlogViewSet
 
+
 router = routers.DefaultRouter()
 router.register(r'blogs', BlogViewSet)
 
