73826f118222cec0ba453bde591d1a2a82d82b28
Jena <jpantano30@gmail.com>
2024-06-18 17:49:50 -0400

    deployment

--- myblog/settings.py
+++ myblog/settings.py
@@ -54,7 +54,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://your-netlify-site.netlify.app",
+]
 
 ROOT_URLCONF = 'myblog.urls'
 
